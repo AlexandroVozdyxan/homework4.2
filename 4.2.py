dc68a76b1dfe754acf63b8965c3a8afd8cc24bd4
vovav <vovavitka10@gmail.com>
2024-08-30 18:42:05 +0300

    4.2

---
+++ 4.2.py
@@ -0,0 +1,9 @@
+numbers = [0, 3, 3, 6, 7, 4, 3, 8]
+even_numbers = numbers[::2]
+last_element = numbers[-1]
+sum(even_numbers)
+result = sum(even_numbers) * last_element
+print(even_numbers)
+print(last_element)
+print(sum(even_numbers))
+print(result)
